cd7a81d04a95280304fb52d6f668bd8903bd0efc
mostafaKeen <mostafa@keenenter.com>
2026-06-30 14:10:27 +0300

    handler 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/bitrix24/callback',
+            '/bitrix24/oauth/callback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
