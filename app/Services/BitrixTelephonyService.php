cc231f5e612ff6a8181e95c7c092f40750fc7b26
mostafaKeen <mostafa@keenenter.com>
2026-06-30 16:29:39 +0300

    call lead 4

--- app/Services/BitrixTelephonyService.php
+++ app/Services/BitrixTelephonyService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\TenantBitrixAccount;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Exception;
 
 class BitrixTelephonyService
